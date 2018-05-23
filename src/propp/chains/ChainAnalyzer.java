b2491ace57532b7d69f3a2f44c57936da6ec3cde
RiccardoCantoni <ricc.cantoni@gmail.com>
2018-05-23 15:48:42 +0200

    chainAnalyzer+, rework Aa

---
+++ src/propp/chains/ChainAnalyzer.java
@@ -0,0 +1,46 @@
+package propp.chains;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import graph.FunctionChain;
+import graph.Node;
+import state.Predicate;
+
+public class ChainAnalyzer {
+	
+	FunctionChain chain;
+	
+	public ChainAnalyzer(FunctionChain c) {
+		this.chain = c;
+	}
+	
+	public ChainAnalyzer(String chainFile) {
+		this.chain = FunctionChain.deserializeFrom(chainFile);
+	}
+	
+	public List<Predicate> getAddedPredicates() {
+		List<Predicate> preds = new LinkedList<>();
+		for (Node n : chain.nodes) {
+			preds.addAll(n.toAdd.set);
+		}
+		return preds;
+	}
+	
+	public List<Predicate> getRequiredPredicates() {
+		List<Predicate> preds = new LinkedList<>();
+		for (Node n : chain.nodes) {
+			preds.addAll(n.preconditions.requiredPredicates());
+		}
+		return preds;
+	}
+	
+	public List<String> getLabels(){
+		List<String> labels = new LinkedList<>();
+		for (Node n : chain.nodes) {
+			labels.add(n.label);
+		}
+		return labels;
+	}
+	
+}
--- src/propp/chains/ChainUpdater.java
+++ src/propp/chains/ChainUpdater.java
@@ -5,6 +5,16 @@
  */
 package propp.chains;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+import graph.FunctionChain;
+import state.AtomMatcher;
+import state.Predicate;
+import state.PredicateMatcher;
+import state.PredicateSet;
+
 /**
  *
  * @author Riccardo
@@ -28,6 +38,39 @@ public class ChainUpdater {
         cg = new TestChainSmall();
         cg.createSave();
         System.out.println("all chains updated successfully");
+        //checkPredicateConsistency();
+    }
+    
+    private static void checkPredicateConsistency() {
+    	ChainAnalyzer ca;
+    	PredicateMatcher m;
+    	PredicateSet added = new PredicateSet();
+    	List<Predicate> required = new LinkedList<>();
+    	List<FunctionChain> chainSeq = Arrays.asList(defaultSequence());
+    	for (FunctionChain c : chainSeq) {
+    		ca = new ChainAnalyzer(c);
+    		for (Predicate p : ca.getAddedPredicates()) {
+    			added.addPredicate(p);
+    		}
+    		required.addAll(ca.getRequiredPredicates());
+    	}
+    	for (Predicate req : required) {
+    		m = new AtomMatcher(req);
+    		if (!m.matchAny(added)) { 
+    			throw new IllegalArgumentException("precondition contains unknown predicate: "+req.toString());
+    		}
+    	}
+    }
+    
+    private static FunctionChain[] defaultSequence(){
+        FunctionChain[] seq = new FunctionChain[6];
+        seq[0] = FunctionChain.deserializeFrom("Aa");
+        seq[1] = FunctionChain.deserializeFrom("BC");
+        seq[2] = FunctionChain.deserializeFrom("DE");
+        seq[3] = FunctionChain.deserializeFrom("F");
+        seq[4] = FunctionChain.deserializeFrom("G");
+        seq[5] = FunctionChain.deserializeFrom("HJ");
+        return seq;
     }
     
 }
