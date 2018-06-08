3c4c76130643bb081722a07c9fcb108c5749a83d
RiccardoCantoni <ricc.cantoni@gmail.com>
2018-06-08 10:58:10 +0200

    filler chain +

---
+++ src/propp/chains/FillerChain.java
@@ -0,0 +1,22 @@
+package propp.chains;
+
+import proppFunction.FunctionChain;
+import proppFunction.Node;
+import proppFunction.NodeType;
+
+public class FillerChain implements ChainGenerator{
+
+	@Override
+	public void createSave() {
+		FunctionChain C = new FunctionChain();
+		C.FunctionName = "Filler";
+		Node n;
+		
+		n = new Node("filler_PLACEHOLDER", NodeType.NONE);
+		C.addNode(n);
+		C.setInitial(n);
+		
+		C.serialize();
+	}
+
+}
