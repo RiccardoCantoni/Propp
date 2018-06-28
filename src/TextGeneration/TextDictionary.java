bf7a38cd01aa7f8d72744d20a321672c52404d3c
Riccardo Cantoni <ricc.cantoni@gmail.com>
2018-06-28 12:02:03 +0200

    dictionary update+

--- src/TextGeneration/TextDictionary.java
+++ src/TextGeneration/TextDictionary.java
@@ -4,25 +4,37 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.PrintWriter;
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+
+import myUtils.JsonDataManager;
+import myUtils.ListUtil;
+import propp.chains.ChainAnalyzer;
+import proppFunction.FunctionChain;
 import proppFunction.SharedRandom;
 
 public class TextDictionary {
 	
 	private Map <String, String[]> tdict;
+	private String filename;
 	
-	public TextDictionary() {
+	public TextDictionary(String filename) {
 		tdict = new HashMap<String, String[]>();
-		mockDict();
+		this.filename = filename;
 	}
 	
-	private void mockDict() {
-		tdict.put("label1", new String[] {"a","b","c"});
-		tdict.put("label3", new String[] {"a"});
-		tdict.put("label2", new String[] {"a","b","c","d"});
-		tdict.put("label4", new String[] {"a","b"});
+	public void mockDict() {
+		tdict.put("la", new String[] {"x","y","z"});
+		tdict.put("lb", new String[] {});
+		tdict.put("lc", new String[] {"t","t"});
+		unloadToCSV(filename);
 	}
 	
 	public void unloadToCSV(String filename) {
@@ -32,7 +44,7 @@ public class TextDictionary {
 		        StringBuilder sb = new StringBuilder();
 		        sb.append(s);
 		        for (String ss : tdict.get(s)) {
-		        	sb.append(";"+ss);
+		        	sb.append(","+ss);
 		        }
 		        sb.append('\n');
 		        pw.write(sb.toString());
@@ -72,5 +84,38 @@ public class TextDictionary {
 		String[] txs = getTexts(label);
 		return txs[rnd.nextInt(txs.length)];
 	}
+	
+	public void updateDictionary() {
+		//List<String> chainLabels = getAllLabels();
+		List<String> chainLabels = Arrays.asList(new String[] {
+				"la",
+				"lb",
+				"lc",
+				"ld"
+		});
+		loadFromCSV(filename);
+		Map <String, String[]> newdict = new HashMap<String, String[]>();
+		for (String label : chainLabels) {
+			if (tdict.containsKey(label)) {
+				newdict.put(label, tdict.get(label));
+			}else {
+				newdict.put(label, new String[0]);
+			}
+		}
+		tdict = newdict;
+		unloadToCSV(filename);
+	}
+	
+	private static List<String> getAllLabels(){
+    	List<String> labels = new LinkedList<>();
+    	ChainAnalyzer ca;
+    	JsonDataManager jdm = new JsonDataManager("data.json");
+    	JsonArray chains = jdm.loadArray("chains");
+    	for (JsonObject o : chains.getValuesAs(JsonObject.class)) {
+    		ca = new ChainAnalyzer(FunctionChain.deserializeFrom(o.getString("name")));
+    		labels.addAll(ca.getLabels());
+    	}
+    	return labels;
+    }
 	
 }
